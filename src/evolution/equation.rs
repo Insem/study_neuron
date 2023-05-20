6be94e56aae4da6b9a665cb09d2a91d4712050b1
Petr Ivanov <pivanov@rvision.pro>
2023-05-20 14:57:54 +0300

    compare

--- src/evolution/equation.rs
+++ src/evolution/equation.rs
@@ -80,7 +80,7 @@ pub fn equation(
     roots: Vec<NeuronCalculateType>,
     equals: EquationInt,
     individuals_count: EquationInt,
-) -> Result<()> {
+) -> Result<i32> {
     let mut _population: Population = Population::new(
         |num| -> EquationInt { (num).trunc() as EquationInt },
         roots.clone(),
@@ -108,7 +108,7 @@ pub fn equation(
                     i,
                     v.1.iter().map(|num| num.trunc()).collect::<Vec<f32>>()
                 );
-                break;
+                return Ok(i);
             }
             None => (),
         };
@@ -124,7 +124,7 @@ pub fn equation(
         )?;
     }
 
-    Ok(())
+    anyhow::anyhow!("Infinity");
 }
 
 fn is_end(selection: &Vec<(EquationInt, &Matrix)>) -> std::option::Option<(i32, Vec<f32>)> {
